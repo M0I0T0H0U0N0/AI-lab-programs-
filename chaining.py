f04e84e50037281a54939b5903dab1a4602d8b2e
MITHUN J MURDESHWAR <121329524+M0I0T0H0U0N0@users.noreply.github.com>
2023-10-17 11:53:34 +0530

    Update chaining.py

--- chaining.py
+++ chaining.py
@@ -33,3 +33,43 @@ if __name__=="__main__":
     kb.add_fact("C")
     kb.apply_forward_chaining()
     print("derived facts",kb.facts)
+
+    knowledge_base ={
+        "rule1":{
+        "if":["A","B"],
+        "then":"C"
+        },
+        "rule2":{
+            "if" :["D"],
+            "then":"A"
+
+        },
+        "rule3":{
+            "if":["E"],
+            "then" :"B"
+        },
+        "rule4":{
+            "if":["F"],
+            "then":"D"
+        },
+        "rule5":{
+            "if":["G"],
+            "then":"E"
+        }
+    }
+    def backward_chaining(goal,known_facts):
+        if goal in known_facts:
+            return True
+        for rule ,value in knowledge_base.items():
+            if goal in value["if"]:
+                all_conditions_met=all(condition in known_facts for condition in value["if"])
+                if all_conditions_met and backward_chaining(value["then"]):
+                    return True
+                return False
+            goal="C"
+            known_facts=["G","F","E"]
+
+            if backward_chaining(goal,known_facts):
+                print(f"the goal '{goal}' can be reached.")
+            else:
+                print(f"the goal 'goal' cannot be reached")
